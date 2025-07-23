38f948940b75441482d0c906989dde0961a4041e
Chithambaranathan <aspirin.gtr@gmail.com>
2025-07-24 00:36:03 +0530

    finished

--- obj/Debug/net8.0/Autoclicker.AssemblyInfo.cs
+++ obj/Debug/net8.0/Autoclicker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Autoclicker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f4a0616ab5aa6e3af77ef625b86098baefa0726")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a70932ae71dd179cdf80c654f94f34cf84492c33")]
 [assembly: System.Reflection.AssemblyProductAttribute("Autoclicker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Autoclicker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
